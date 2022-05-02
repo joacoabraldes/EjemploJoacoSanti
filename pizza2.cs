a19a88ca164213b7e47651487beb02e03a354155
santiyoo <101531142+santiyoo@users.noreply.github.com>
2022-05-02 10:24:01 -0300

    Update pizza2.cs

--- pizza2.cs
+++ pizza2.cs
@@ -3,7 +3,7 @@ using System;
 namespace Pizza.API.Models {
     public class Pizza {
         public int      Id              { get; set; }
-        public string   Nombre          { get; set; }
+        public string   Nombre          { get; set; } // aca es ...
         public bool     LibreGluten     { get; set; }
         public float    Importe         { get; set; }
         public string   Descripcion     { get; set; }
