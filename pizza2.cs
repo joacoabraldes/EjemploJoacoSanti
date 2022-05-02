6aa1f6e07d435eeb2fbb7bee77cbf86344141d4b
SANTINO DAVID YOO <46754432@ort>
2022-05-02 10:22:57 -0300

    puse modificariocn s

--- pizza2.cs
+++ pizza2.cs
@@ -4,8 +4,9 @@ namespace Pizza.API.Models {
     public class Pizza {
         public int      Id              { get; set; }
         public string   Nombre          { get; set; }
-        public bool     LibreGluten     { get; set; }
+        public bool     LibreGlutens    { get; set; }
         public float    Importe         { get; set; }
         public string   Descripcion     { get; set; }
+        public string   Observaciones   { get; set; } //es
     }
 }
